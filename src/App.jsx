3f5409c7bf83e9a37b3fa2cd7c87c62f81da425e
YULIIA M <yuliiamartynovych@gmail.com>
2025-06-18 12:32:50 +0200

    add Appi.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import './App.scss';
 import { PostList } from './components/PostList/PostList';
 
-// import postsFromServer from './api/posts.json';
-// import commentsFromServer from './api/comments.json';
-// import usersFromServer from './api/users.json';
+import postsFromServer from './api/posts.json';
+import commentsFromServer from './api/comments.json';
+import usersFromServer from './api/users.json';
 
-export const App = () => (
-  <section className="App">
-    <h1 className="App__title">Static list of posts</h1>
-    <PostList />
-  </section>
-);
+export const App = () => {
+  return (
+    <section className="App">
+      <h1 className="App__title">Static list of posts</h1>
+      <PostList
+        posts={postsFromServer}
+        users={usersFromServer}
+        comments={commentsFromServer}
+      />
+    </section>
+  );
+};
